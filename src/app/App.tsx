3904e4dcce18a98f34433857d5c4de4a0a7a4cd0
askrzypczynska <aleksandraskrzypczynska@gmail.com>
2022-11-10 15:15:21 +0100

    Adding props and hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,20 @@
 import React from "react"
+import styled, { ThemeProvider } from "styled-components"
+import { TranslatorScreen } from "../features/translator"
+import { theme } from "./lib/styles"
 
 export const App = () => {
     return(
-        <div>
-            Hello World!
-        </div>
+        <ThemeProvider theme={ theme }>
+            <AppContainer>
+                <TranslatorScreen />
+            </AppContainer>
+        </ThemeProvider>
     )
 }
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.colors.background}
+`
---
+++ src/features/translator/TranslatorScreen.tsx
@@ -0,0 +1,18 @@
+import React, { useTransition } from "react"
+import styled from "styled-components"
+import { useTranslations } from "../../app/lib/hooks"
+
+
+export const TranslatorScreen: React.FunctionComponent = () => {
+    const T = useTranslations()
+    
+    return(
+    <Container>
+        {T.appName}
+    </Container>
+    )
+}
+
+const Container = styled.div`
+    color: ${({ theme }) => theme.colors.typography}
+`
